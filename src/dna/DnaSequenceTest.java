b2a8ef532face64bf73b2f95ae7855ef319713f5
Emily Schroeder <emily.schroeder@protonmail.com>
2020-05-04 21:53:14 -0600

    adding test cases

--- src/dna/DnaSequenceTest.java
+++ src/dna/DnaSequenceTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DnaSequenceTest {
     DnaSequence dnaSequence;
     @Before
@@ -14,7 +17,51 @@ public class DnaSequenceTest {
 
     @Test
     public void transcribe() {
-        // TODO: implement tests
+    	//creating a list to hold our expected answers
+    	List<String> expected = new LinkedList<String>();
+
+    	//testing we can transcribe one protein
+    	String dna = "GCT";
+		expected.add("ala");
+    	assertEquals(expected, dnaSequence.transcribe(dna));	
+    }
+    
+    @Test
+    public void transcribeLargeSeq() {
+    	List<String> expected = new LinkedList<String>();
+    	
+    	//trying a longer dna sequence
+    	String dna = "GCTCGCAACATCGGATATAGA";
+    	expected.add("ala");
+    	expected.add("arg");
+    	expected.add("asn");
+    	expected.add("lle");
+    	expected.add("gly");
+    	expected.add("tyr");
+    	expected.add("arg");
+    	
+    	assertEquals(expected, dnaSequence.transcribe(dna));	
+    }
+    
+    
+    @Test
+    public void transcribeNewSq() {
+    	//and just for fun
+    	List<String> expected = new LinkedList<String>();
+    	
+    	//even though this technically stops at the second triplet :)
+    	String dna = "TACTGATCGACCCCCATAATGAAA";
+    	
+    	expected.add("tyr");
+    	expected.add("stop");
+    	expected.add("ser");
+    	expected.add("thr");
+    	expected.add("pro");
+    	expected.add("lle");
+    	expected.add("met");
+    	expected.add("lys");
+    	
+    	assertEquals(expected, dnaSequence.transcribe(dna));
     	
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         String dna = "GCTCGCAACATCGGATATAGA";
-        System.out.println("DNA: " + dna);
+    	System.out.println("DNA: " + dna);
         System.out.println("The amino acids produced by this DNA Sequence is:");
 
         DnaSequence dnaSequence = new DnaSequence();
