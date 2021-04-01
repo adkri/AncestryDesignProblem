1062bd372064ee6e9506d9da64de659d76455438
ewbrees <81719511+ewbrees@users.noreply.github.com>
2021-03-31 19:32:55 -0500

    Fixed DnaSequence and added tests

--- src/dna/DnaSequenceTest.java
+++ src/dna/DnaSequenceTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 public class DnaSequenceTest {
     DnaSequence dnaSequence;
     @Before
@@ -15,5 +17,30 @@ public class DnaSequenceTest {
     @Test
     public void transcribe() {
         // TODO: implement tests
+    	DnaSequence test = new DnaSequence();
+    	String testdna = "GCTCGTTCTACT"; // work with a variety of codons
+    	LinkedList<String> testlist = new LinkedList<String>();
+    	testlist.add("ala");
+    	testlist.add("arg");
+    	testlist.add("ser");
+    	testlist.add("thr");
+    	assertEquals(testlist, test.transcribe(testdna));
+    	
+    	testdna = "GCTCGTTCTAC"; //skip last codons if there is only a double
+    	testlist.remove("thr");
+    	assertEquals(testlist, test.transcribe(testdna));
+    	
+    	testdna = "GCTCGTTCTA"; //or single
+    	assertEquals(testlist, test.transcribe(testdna));
+    	
+    	testdna = "GCTCGTTCT"; //work properly if there are no extra codons
+    	assertEquals(testlist, test.transcribe(testdna));
+    	
+    	testdna = "blaGCTCGTTCTblabl";
+    	assertEquals(testlist, test.transcribe(testdna)); //don't add triples that don't qualify as codons
+    	
+    	LinkedList<String> testlist2 = new LinkedList<String>();
+    	String testdna2 = "TT"; //do nothing if there is less then a triple codon
+    	assertEquals(testlist2, test.transcribe(testdna2));
+    	}
     }
-}
