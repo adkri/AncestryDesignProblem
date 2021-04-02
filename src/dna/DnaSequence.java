90a9b5efc89c527921f709c79931d4a587cba868
jwosborne-knox <jwosborne@knox.edu>
2021-04-02 16:49:09 -0500

    Fixed the bug in DnaSequence

--- src/dna/DnaSequence.java
+++ src/dna/DnaSequence.java
@@ -11,18 +11,19 @@ public class DnaSequence {
     }
 
     public List<String> transcribe(String dna) {
-        // TODO: fix me
         List<String> aminoAcids = new LinkedList<>();
         int i = 0;
         while(i < dna.length()) {
-            String triplet = "" + dna.charAt(i) + dna.charAt(i+1) + dna.charAt(i+2);
-            try {
-                String acid = this.dnaCodon.acidFor(triplet);
-                aminoAcids.add(acid);
-            } catch (Exception e) {
-                // silently pass codon that does not transcribe a amino acid
-            }
-            i += 1;
+        	if(i < 19) {
+		        String triplet = "" + dna.charAt(i) + dna.charAt(i+1) + dna.charAt(i+2);
+		        try {
+		            String acid = this.dnaCodon.acidFor(triplet);
+		            aminoAcids.add(acid);
+		        } catch (Exception e) {
+		            // silently pass codon that does not transcribe a amino acid
+		        }
+		        i += 1;
+        	}
         }
         return aminoAcids;
     }
