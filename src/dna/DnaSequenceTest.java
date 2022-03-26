188af40cd4684c0ff99fc986d43ab8de9b4cc845
BrendanC-S <102436259+BrendanC-S@users.noreply.github.com>
2022-03-26 16:11:10 -0600

    bugfix
    fixed bug causing too many proteins to be returned + indexoutofbounds error

--- src/dna/DnaSequenceTest.java
+++ src/dna/DnaSequenceTest.java
@@ -3,7 +3,6 @@ package dna;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class DnaSequenceTest {
     DnaSequence dnaSequence;
@@ -14,6 +13,6 @@ public class DnaSequenceTest {
 
     @Test
     public void transcribe() {
-        // TODO: implement tests
+
     }
 }
