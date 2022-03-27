aba59b42ec4058ecf2f0585e9d357d20e0274e79
khhernandez <khhernandez@knox.edu>
2022-03-27 15:32:31 -0500

    Fixed Codon reading

--- src/dna/DnaSequenceTest.java
+++ src/dna/DnaSequenceTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DnaSequenceTest {
     DnaSequence dnaSequence;
     @Before
@@ -15,5 +18,23 @@ public class DnaSequenceTest {
     @Test
     public void transcribe() {
         // TODO: implement tests
+    	List<String> answ = new LinkedList<>();
+    	answ.add("asp");
+    	answ.add("leu");
+    	System.out.println(dnaSequence.transcribe("GATTTACG"));
+    	assertEquals(answ, dnaSequence.transcribe("GATTTACG"));
+    	
+    	List<String> answ2 = new LinkedList<>();
+    	answ2.add("met");
+    	answ2.add("tyr");
+    	answ2.add("arg");
+    	answ2.add("ala");
+    	answ2.add("asp");
+    	answ2.add("asp");
+    	answ2.add("asn");
+    	System.out.println(dnaSequence.transcribe("ATGTATAGAGCTGATGATAAC"));
+    	assertEquals(answ2, dnaSequence.transcribe("ATGTATAGAGCTGATGATAAC"));
+    	
+    	
     }
 }
