821f00b0f80009b357c36c027c6cc0192bf90b20
Juliana-Number22Knox <jevinluan22@gmail.com>
2022-03-27 22:06:37 -0500

    Update DnaSequence.java

--- src/dna/DnaSequence.java
+++ src/dna/DnaSequence.java
@@ -14,12 +14,13 @@ public class DnaSequence {
         // TODO: fix me
         List<String> aminoAcids = new LinkedList<>();
         int i = 0;
-        while(i < dna.length()) {
+        while(i < dna.length()-3) {
             String triplet = "" + dna.charAt(i) + dna.charAt(i+1) + dna.charAt(i+2);
             try {
                 String acid = this.dnaCodon.acidFor(triplet);
                 aminoAcids.add(acid);
             } catch (Exception e) {
+            	
                 // silently pass codon that does not transcribe a amino acid
             }
             i += 1;
